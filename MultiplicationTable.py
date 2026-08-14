1486eb73cf725ab970203bdcecbe342ba461ee9e
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-08-14 13:37:24 +0530

    Add multiplication table program

---
+++ MultiplicationTable.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+print("Multiplication Table of", num)
+
+for i in range(1, 11):
+    result = num * i
+    print(num, "x", i, "=", result)
+
+print("Table completed!")
+
+print("Thank you for using the program.")
